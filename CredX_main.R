b665e33ed4e4131f34dd0d0236cd07fbee6ae652
Amit Bhosale <bhosaleamit.2007@gmail.com>
2019-01-21 19:50:07 +0530

    Updated the code with setting the working directory to the active R file and reading the files from read.csv base R package.

--- CredX_main.R
+++ CredX_main.R
@@ -1,17 +1,31 @@
 # CredX - Acquisition Risk Analysis Capstone Project.
 
-# Read the datasets
-library(readr)
+library(rstudioapi)
+#Set working directory to directory of the file
 
-Demographic_data <- read_csv("Demographic data.csv")
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+getwd()
+
+# Read the datasets
+
+Demographic_data <- read.csv("Demographic data.csv")
 
 # Demographic data contains customer level information (age, gender, income, marital status, etc)
 
 View(Demographic_data)
 
-Credit_Bureau_data <- read_csv("Credit Bureau data.csv")
+Credit_Bureau_data <- read.csv("Credit Bureau data.csv")
 
 # Credit Bureau Data contains information taken from credit bureau. ('number of times 30 DPD or worse in last 3/6/12 months', 'outstanding balance', 'number of trades', etc.) 
 
 View(Credit_Bureau_data)
+
+# Data Cleaning - Demographic data file.
+
+summary(Demographic_data)
+
+# Identify the null values or na values.
+
+sum(is.na(Demographic_data))
+# 1428 null values found in the Demographic dataset.
 
