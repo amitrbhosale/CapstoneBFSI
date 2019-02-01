e58cf500c55dc1366a0d25c896b6f02b1e039b7e
Pradeep Doddarangaiah <pradeep.doddarangaiah@gmail.com>
2019-02-01 18:45:11 +0530

    WOE and IV for merged data, replaced the columns with respective WOE values and obtained the final WOE_data_final dataframe which has only important variables based on IV value

--- CredX_main.R
+++ CredX_main.R
@@ -4,9 +4,13 @@
 install.packages("rstudioapi")
 install.packages("ggplot2")
 install.packages("Information")
+install.packages("stringr")
+install.packages("fuzzyjoin")
 library("Information")
 library(rstudioapi)
 library(ggplot2)
+library(stringr)
+library(fuzzyjoin)
 
 #Set working directory to directory of the file
 
@@ -90,6 +94,10 @@ sum(ifelse(merged_df$Performance.Tag.x == merged_df$Performance.Tag.y, 1,0)  | i
 # Removing the redundant column
 merged_df$Performance.Tag.x <- NULL
 
+#Setting the negative values to null
+
+merged_df[merged_df < 0] <- NA
+
 #------------------------------------------EDA to find the important variables-----------------------------------
 
 #Filtering only defaulters data
@@ -105,15 +113,11 @@ ggplot(EDA_data, aes(x=Gender,fill=factor(Performance.Tag.y)))+geom_bar(position
 
 ggplot(EDA_data, aes(x=EDA_data$Marital.Status..at.the.time.of.application.,fill=factor(Performance.Tag.y)))+geom_bar(position = "fill")
 
-ggplot(EDA_data, aes(x=EDA_data$Marital.Status..at.the.time.of.application.,fill=factor(Performance.Tag.y)))+geom_bar(position = "fill")
-
 ggplot(EDA_data, aes(x=factor(EDA_data$No.of.dependents),fill=factor(Performance.Tag.y)))+geom_bar(position = "fill")
 
 ggplot(EDA_data, aes(x=factor(EDA_data$Education),fill=factor(Performance.Tag.y)))+geom_bar(position = "fill")
 
 ggplot(EDA_data, aes(x=factor(EDA_data$Profession),fill=factor(Performance.Tag.y)))+geom_bar(position = "fill")
-
-ggplot(EDA_data, aes(x=factor(EDA_data$Education),fill=factor(Performance.Tag.y)))+geom_bar(position = "fill")
 
 ggplot(EDA_data, aes(x=factor(EDA_data$Type.of.residence),fill=factor(Performance.Tag.y)))+geom_bar(position = "fill")
 
@@ -189,29 +193,25 @@ summary(merged_df$Performance.Tag.y)
 #Remove NAs from Dependant Variable as it won't allow execution of IV functions.
 WOE_Data <- subset(merged_df, is.na(merged_df$Performance.Tag.y)==FALSE)
 WOE_Data$Performance.Tag.y <- as.numeric(WOE_Data$Performance.Tag.y)
-WOE_Data[which(WOE_Data[]<0)] <- NULL
 
 #Converting categorical variables to factor
 WOE_Data$Presence.of.open.auto.loan <- factor(WOE_Data$Presence.of.open.auto.loan)
 WOE_Data$Presence.of.open.home.loan <- factor(WOE_Data$Presence.of.open.home.loan)
-
-#Removing application ID column
-WOE_Data <- WOE_Data[,-1]
 
 # Generate InfoTables for the variables
 IV <- create_infotables(WOE_Data,y="Performance.Tag.y", parallel = TRUE)
 
 IV$Summary
 #It is evident from summary of IV table that following attributes are not significant and can be rejected in the model
-#24                                      Presence.of.open.home.loan 1.761939e-02
-#1                                                              Age 3.350241e-03
-#4                                                 No.of.dependents 2.653501e-03
-#7                                                       Profession 2.219893e-03
-#27                                      Presence.of.open.auto.loan 1.658061e-03
-#8                                                Type.of.residence 9.198065e-04
-#6                                                        Education 7.825416e-04
-#2                                                           Gender 3.258695e-04
-#3                      Marital.Status..at.the.time.of.application. 9.473857e-05
+#25                                      Presence.of.open.home.loan 1.761939e-02
+#2                                                              Age 3.350241e-03
+#5                                                 No.of.dependents 2.653501e-03
+#8                                                       Profession 2.219893e-03
+#28                                      Presence.of.open.auto.loan 1.658061e-03
+#9                                                Type.of.residence 9.198065e-04
+#7                                                        Education 7.825416e-04
+#3                                                           Gender 3.258695e-04
+#4                      Marital.Status..at.the.time.of.application. 9.473857e-05
 
 # Plot IVs for prominent variables obtained from IV's summary
 plot_infotables(IV, IV$Summary$Variable[5],same_scales = TRUE)
@@ -225,3 +225,64 @@ plot_infotables(IV, IV$Summary$Variable[25:26],same_scales = TRUE)
 
 # Get WOE values for all 10 bins for Age variable
 IV$Tables
+
+#Function to replace column values with WOE value
+woe_replace <- function(df_orig, IV) {
+  df <- cbind(df_orig)
+  df_clmtyp <- data.frame(clmtyp = sapply(df, class))
+  df_col_typ <-
+    data.frame(clmnm = colnames(df), clmtyp = df_clmtyp$clmtyp)
+  for (rownm in 1:nrow(df_col_typ)) {
+    colmn_nm <- toString(df_col_typ[rownm, "clmnm"])    
+    if(colmn_nm %in% names(IV$Tables)){
+      column_woe_df <- cbind(data.frame(IV$Tables[[toString(df_col_typ[rownm, "clmnm"])]]))
+      if (df_col_typ[rownm, "clmtyp"] == "factor" | df_col_typ[rownm, "clmtyp"] == "character") {
+        df <-
+          dplyr::inner_join(
+            df,
+            column_woe_df[,c(colmn_nm,"WOE")],
+            by = colmn_nm,
+            type = "inner",
+            match = "all"
+          )
+        df[colmn_nm]<-NULL
+        colnames(df)[colnames(df)=="WOE"]<-colmn_nm
+      } else if (df_col_typ[rownm, "clmtyp"] == "numeric" | df_col_typ[rownm, "clmtyp"] == "integer") {
+        column_woe_df$lv<-as.numeric(str_sub(
+          column_woe_df[,colmn_nm],
+          regexpr("\\[", column_woe_df[,colmn_nm]) + 1,
+          regexpr(",", column_woe_df[,colmn_nm]) - 1
+        ))
+        column_woe_df$uv<-as.numeric(str_sub(
+          column_woe_df[,colmn_nm],
+          regexpr(",", column_woe_df[,colmn_nm]) + 1,
+          regexpr("\\]", column_woe_df[,colmn_nm]) - 1
+        ))
+        column_woe_df[colmn_nm]<-NULL      
+        column_woe_df<-column_woe_df[,c("lv","uv","WOE")]      
+        colnames(df)[colnames(df)==colmn_nm]<-"WOE_temp2381111111111111697"      
+        df <-
+          fuzzy_inner_join(
+            df,
+            column_woe_df[,c("lv","uv","WOE")],
+            by = c("WOE_temp2381111111111111697"="lv","WOE_temp2381111111111111697"="uv"),
+            match_fun=list(`>=`,`<=`) 
+          )      
+        df["WOE_temp2381111111111111697"]<-NULL      
+        df["lv"]<-NULL      
+        df["uv"]<-NULL      
+        colnames(df)[colnames(df)=="WOE"]<-colmn_nm      
+      }}
+  }
+  return(df)
+}
+
+#Calling the above function to replace values with WOE
+
+#Obtaining a data frame with only important variables (obtained from IV)
+temp <- WOE_Data[,-c(1:5,7:9,25,28)]
+
+WOE_Data_final <- woe_replace(temp, IV)
+#WOE_Data_final gives the final model after replacing the values with corresponding WOE values
+
+str(WOE_Data_final)
