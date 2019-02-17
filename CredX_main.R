8034aa273f4d11ad7142e61e3e8c4ff8499d5c4b
PradeepDoddarangaiah <Pradeep.Doddarangaiah@gmail.com>
2019-02-17 13:15:58 +0530

    Added Demographic data logistic model building code and application score card code

--- CredX_main.R
+++ CredX_main.R
@@ -911,3 +911,301 @@ cutoff_score <- Offset + (Factor*cutoff_odds)
 cutoff_score
 #Cut off Score is 246.7
 
+#-------------------------------Demographic data WOE, IV and model building-------
+#Need to perform WOE and IV Analysis
+
+summary(Demographic_data$Performance.Tag)
+
+length(which(Demographic_data$Performance.Tag==1))
+length(which(Demographic_data$Performance.Tag==0))
+
+Demographic_data$Performance.Tag <- factor(as.character(Demographic_data$Performance.Tag))
+
+#Swapping 0 and 1 values for performance tag column since 1 should denote good and 0 should denote bad
+for (i in 1:nrow(Demographic_data)) {
+  ifelse(Demographic_data$Performance.Tag[i]==0,Demographic_data$Performance.Tag[i] <- 1,Demographic_data$Performance.Tag[i] <- 0)
+}
+length(which(Demographic_data$Performance.Tag==1))
+length(which(Demographic_data$Performance.Tag==0))
+
+#Remove NAs from Dependant Variable as it won't allow execution of IV functions.
+traindata_dem <- subset(Demographic_data, is.na(Demographic_data$Performance.Tag)==FALSE)
+
+traindata_dem$Performance.Tag <- as.numeric(as.character(traindata_dem$Performance.Tag))
+
+# Generate InfoTables for the variables
+IV_dem <- create_infotables(traindata_dem,y="Performance.Tag",parallel = TRUE, ncore = 4)
+
+IV_dem$Summary
+
+#Adding a bar graph to see the important variablles based on IV value
+All_IV_dem <- data.frame(IV_dem$Summary)
+All_IV_dem$Variable <- factor(All_IV_dem$Variable, levels = All_IV_dem$Variable[order(-All_IV_dem$IV)])
+ggplot(All_IV_dem, aes(x=All_IV_dem$Variable,y=All_IV_dem$IV))+geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + geom_hline(yintercept = 0.1, color = "red")  + xlab("Predictor Variable") + ylab("IV") + ggtitle("IV for Predictor variable")
+
+#Since the IV values of all attributes lie below 0.1, we are considering weak indicators i.e 0.02 to 0.1 for model building
+predictor_variables_dem <- data.frame(IV_dem$Summary)
+predictor_variables_dem <- subset(predictor_variables_dem,predictor_variables_dem$IV >0.02)
+
+predictor_variables_dem
+
+sapply(Demographic_data, function(x) sum(is.na(x)))
+
+# Checking the first predictor variable No.of.months.in.current.residence
+
+IV_dem$Tables$No.of.months.in.current.residence
+
+# Checking the second predictor variable Income
+
+# Checking if the Income variable has outliers.
+
+quantile(Demographic_data$Income, seq(0,1,0.01))
+
+# We can see a jump in the Income variable from -0.5 to 4.5 and Income cannot be negative, therefore we are treating this as an outlier.
+
+Demographic_data$Income <- ifelse(Demographic_data$Income<4.5, 4.5, Demographic_data$Income)
+
+# Checking the third predictor variable No.of.months.in.current.company
+
+IV_dem$Tables$No.of.months.in.current.company
+
+traindata_dem <- Demographic_data
+
+# Create a dataframe with the important variables identified and the dependant variable
+
+impvar_dem_df <- traindata_dem[,c(as.vector(predictor_variables_dem$Variable),"Performance.Tag")]
+
+# WOE replacement in the impvar_df to be carried out.
+# Replacing the woe values for predictor variables
+
+rows_dem_df <- nrow(impvar_dem_df)
+
+#No.of.months.in.current.residence
+
+IVTable <- data.frame(IV_dem$Tables$No.of.months.in.current.residence)
+
+IVTable$min1 <- str_locate(IVTable$No.of.months.in.current.residence,"\\[")[,1]
+IVTable$min2 <- str_locate(IVTable$No.of.months.in.current.residence,"\\,")[,1]
+
+IVTable$min <- as.numeric(substr(IVTable$No.of.months.in.current.residence,IVTable$min1+1, IVTable$min2-1))
+
+IVTable$max1 <- str_locate(IVTable$No.of.months.in.current.residence,"\\,")[,1]
+IVTable$max2 <- str_locate(IVTable$No.of.months.in.current.residence,"\\]")[,1]
+
+IVTable$max <- as.numeric(substr(IVTable$No.of.months.in.current.residence, IVTable$max1+1, IVTable$max2-1))
+
+# Perform following action for every row - for replacement
+for(j in 1: rows_dem_df)
+{
+  val <- impvar_dem_df$No.of.months.in.current.residence[j]
+  woepos <- which(val>= IVTable$min & val <= IVTable$max)
+  woeval <- as.numeric(IVTable$WOE[woepos])
+  #Replacement
+  impvar_dem_df$No.of.months.in.current.residence[j] <- woeval
+}
+
+sum(is.na(impvar_dem_df$No.of.months.in.current.residence))
+
+#For Income
+
+IVTable <- data.frame(IV_dem$Tables$Income)
+
+IVTable$min1 <- str_locate(IVTable$Income,"\\[")[,1]
+IVTable$min2 <- str_locate(IVTable$Income,"\\,")[,1]
+
+IVTable$min <- as.numeric(substr(IVTable$Income,IVTable$min1+1, IVTable$min2-1))
+
+IVTable$max1 <- str_locate(IVTable$Income,"\\,")[,1]
+IVTable$max2 <- str_locate(IVTable$Income,"\\]")[,1]
+
+IVTable$max <- as.numeric(substr(IVTable$Income, IVTable$max1+1, IVTable$max2-1))
+
+# Perform following action for every row - for replacement
+for(j in 1: rows_dem_df)
+{
+  val <- impvar_dem_df$Income[j]
+  woepos <- which(val>= IVTable$min & val <= IVTable$max)
+  woeval <- as.numeric(IVTable$WOE[woepos])
+  #Replacement
+  impvar_dem_df$Income[j] <- woeval
+}
+
+sum(is.na(impvar_dem_df$Income))
+
+#No.of.months.in.current.company
+
+IVTable <- data.frame(IV_dem$Tables$No.of.months.in.current.company)
+
+IVTable$min1 <- str_locate(IVTable$No.of.months.in.current.company,"\\[")[,1]
+IVTable$min2 <- str_locate(IVTable$No.of.months.in.current.company,"\\,")[,1]
+
+IVTable$min <- as.numeric(substr(IVTable$No.of.months.in.current.company,IVTable$min1+1, IVTable$min2-1))
+
+IVTable$max1 <- str_locate(IVTable$No.of.months.in.current.company,"\\,")[,1]
+IVTable$max2 <- str_locate(IVTable$No.of.months.in.current.company,"\\]")[,1]
+
+IVTable$max <- as.numeric(substr(IVTable$No.of.months.in.current.company, IVTable$max1+1, IVTable$max2-1))
+
+# Perform following action for every row - for replacement
+for(j in 1: rows_dem_df)
+{
+  val <- impvar_dem_df$No.of.months.in.current.company[j]
+  woepos <- which(val>= IVTable$min & val <= IVTable$max)
+  woeval <- as.numeric(IVTable$WOE[woepos])
+  #Replacement
+  impvar_dem_df$No.of.months.in.current.residence[j] <- woeval
+}
+
+sum(is.na(impvar_dem_df$No.of.months.in.current.company))
+
+# Modelling Logistic regression for Demographic dataset
+
+########################################################################
+# splitting the data between train and test
+set.seed(100)
+
+impvar_dem_without_NA_df <- subset(impvar_dem_df,is.na(impvar_dem_df$Performance.Tag)==FALSE)
+
+indices = sample.split(impvar_dem_without_NA_df$Performance.Tag, SplitRatio = 0.7)
+
+train = impvar_dem_without_NA_df[indices,]
+
+test = impvar_dem_without_NA_df[!(indices),]
+
+#Initial model
+model_1 = glm(Performance.Tag ~ ., data = train, family = "binomial")
+summary(model_1)
+
+# Using StepAIC function
+model_2<- stepAIC(model_1, direction="both")
+
+summary(model_2)
+vif(model_2)
+
+final_model_dem <- model_2
+
+#---------------------------------------------------------    
+
+# Predicting probabilities of defaulting for the test data
+
+predictions_logit_dem <- predict(final_model_dem, newdata = test[, -4], type = "response")
+summary(predictions_logit_dem)
+
+#--------------------------------------------------------- 
+
+## Model Evaluation: Logistic Regression
+
+# Let's use the probability cutoff of 90.5%.
+
+predicted_Performance_tag <- factor(ifelse(predictions_logit_dem >= 0.93, "no", "yes"))
+test$Performance.Tag <- factor(ifelse(test$Performance.Tag ==1, "no","yes"))
+
+# Creating confusion matrix for identifying the model evaluation.
+
+conf_dem <- confusionMatrix(predicted_Performance_tag, test$Performance.Tag, positive = "no")
+
+conf_dem
+
+#-----------------------------------------------------------
+
+# Let's find out the optimal probalility cutoff 
+
+perform_fn_dem <- function(cutoff) 
+{
+  predicted_Performance_tag <- factor(ifelse(predictions_logit_dem >= cutoff, "no", "yes"))
+  conf <- confusionMatrix(predicted_Performance_tag, test$Performance.Tag, positive = "no")
+  acc <- conf$overall[1]
+  sens <- conf$byClass[1]
+  spec <- conf$byClass[2]
+  out <- t(as.matrix(c(sens, spec, acc))) 
+  colnames(out) <- c("sensitivity", "specificity", "accuracy")
+  return(out)
+}
+
+#---------------------------------------------------------  
+# Creating cutoff values from 0.01 to 0.99 for plotting and initiallizing a matrix of 1000 X 4.
+
+s = seq(0.93,0.99,length=100)
+
+OUT = matrix(0,100,3)
+
+for(i in 1:100)
+{
+  OUT[i,] = perform_fn_dem(s[i])
+} 
+
+#---------------------------------------------------------    
+
+# plotting cutoffs 
+plot(s, OUT[,1],xlab="Cutoff",ylab="Value",cex.lab=1.5,cex.axis=1.5,ylim=c(0,1),type="l",lwd=2,axes=FALSE,col=2)
+axis(1,seq(0.93,1,length=5),seq(0.93,1,length=5),cex.lab=1.5)
+axis(2,seq(0,1,length=5),seq(0,1,length=5),cex.lab=1.5)
+lines(s,OUT[,2],col="darkgreen",lwd=2)
+lines(s,OUT[,3],col=4,lwd=2)
+box()
+legend(0,.50,col=c(2,"darkgreen",4,"darkred"),lwd=c(2,2,2,2),c("Sensitivity","Specificity","Accuracy"))
+
+#---------------------------------------------------------    
+cutoff_dem <- s[which(abs(OUT[,1]-OUT[,2])<0.01)]
+
+# Let's choose a cutoff value of 95.66% for final model
+
+predicted_Performance_tag <- factor(ifelse(predictions_logit_dem >= .9566, "no", "yes"))
+
+conf_final <- confusionMatrix(predicted_Performance_tag, test$Performance.Tag, positive = "no")
+
+acc <- conf_final$overall[1]
+
+sens <- conf_final$byClass[1]
+
+spec <- conf_final$byClass[2]
+
+acc
+
+sens
+
+spec
+
+#----------------------------End_of_model_buiding----------------------------------------
+
+#----------------------------Creation of application scorecard for demographic dataset--------
+
+Application_Card_dem_Merged <- impvar_dem_df
+
+predictions_logit_dem <- predict(final_model_dem, newdata = Application_Card_dem_Merged[, -4], type = "response")
+predicted_Performance_tag <- factor(ifelse(predictions_logit_dem >= 0.9566, "no", "yes"))
+
+# Appending the probabilities and response variables to the test data
+
+Application_Card_dem_Merged$predicted_probs <- predictions_logit_dem
+
+Application_Card_dem_Merged$predicted_Performance_tag <- predicted_Performance_tag
+
+Application_Card_dem_Merged$predict_NonDefault <- Application_Card_dem_Merged$predicted_probs
+
+Application_Card_dem_Merged$predict_Default <- 1 - Application_Card_dem_Merged$predict_NonDefault
+
+Application_Card_dem_Merged$Odds <-  log(Application_Card_dem_Merged$predict_NonDefault/Application_Card_dem_Merged$predict_Default)
+
+# Score = Offset + ( Factor * log(odds) )
+# Factor = PDO/ln(2)
+# Offset = Score-(Factor*log(odds))
+# PDO = 20, Base Score=400, odds = 10
+
+Factor = 20/log(2)
+#28.8539
+
+Offset = 400 - (28.8539*log(10))
+
+Application_Card_dem_Merged$Score = Offset + (Factor*Application_Card_dem_Merged$Odds)
+
+#Calculating the cut off score for application score
+
+cutoff_odds_dem <- log((1-0.9566)/0.9566)
+cutoff_score_dem <- Offset + (Factor*cutoff_odds_dem)
+cutoff_score_dem
+#Cut off Score is 244.31
+
+#---------------End of application Score card code for Demographic data----------------
+
+
